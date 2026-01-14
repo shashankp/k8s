ec230cefe1ea2f0fa10c39c82765222704c50ac3
shashankp <shashank.pv@gmail.com>
2026-01-14 13:42:50 +0530

    csparser

---
+++ parser/CsParser/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+
+MSBuildLocator.RegisterDefaults();
+using var workspace = MSBuildWorkspace.Create();
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: CsParser <path-to-solution>");
+    return;
+}
+
+var solutionPath = args[0];
+var solution = await workspace.OpenSolutionAsync(solutionPath);
+foreach (var project in solution.Projects)
+{
+    Console.WriteLine($"Project: {project.Name}");
+}
